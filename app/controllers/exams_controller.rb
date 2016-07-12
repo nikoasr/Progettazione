f4f9f2ac70ced380870c738461fd4b42bab1204c
nicoasr <thealligator95@hotmail.it>
2016-07-12 01:22:27 +0000

    added admin functions

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,25 +1,53 @@
 class ExamsController < ApplicationController
     before_action :current_user, only: [:show]
     
-    before_action :find_exam, only: [:show]
+    before_action :find_exam, only: [:show, :edit, :update, :destroy]
     
     
     def index
         if params[:anno].blank?
-			@exams = Exam.all.order("created_at DESC")
+			@exams = Exam.all.order("nome")
 		else
-			@exams = Exam.where(:anno => params[:anno]).order("created_at DESC")
+			@exams = Exam.where(:anno => params[:anno]).order("nome")
+		end
+    end
+    
+    def new
+        @exam = Exam.new
+    end    
+    
+    def create
+        @exam = Exam.new(exam_params)
+        if @exam.save
+			redirect_to root_path
+		else
+			render 'new'
 		end
     end
     
     def show
-        
     end
+    
+    def edit
+    end
+    
+    def update
+		if @exam.update(exam_params)
+			redirect_to exam_path(@exam)
+		else
+			render 'edit'
+		end
+	end
+    
+    def destroy
+		@exam.destroy
+		redirect_to root_path
+	end
     
     private
        
         def exam_params
-			params.require(:exam).permit(:nome, :anno)
+			params.require(:exam).permit(:nome, :anno, :professore, :sito)
 		end
 
 		def find_exam
