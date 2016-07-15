69e877a58650832e65acb8be55f0fb62c2569950
nicoasr <thealligator95@hotmail.it>
2016-07-15 17:58:15 +0000

    add forms validations

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,6 +1,10 @@
 class Tutor < ActiveRecord::Base
     belongs_to :exam
     
-    validates :cel, :email, :presence => true
+    validates :cel, :presence => true, numericality: { only_integer: true }
+    validates :email,   
+        :presence => true,
+        :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
+        
 
 end
